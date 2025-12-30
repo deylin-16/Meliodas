d179ab1e9643e5bce705822d562f6d415319be6d
deylin-16 <deylin1616@gmail.com>
2025-12-29 23:20:50 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -9,28 +9,26 @@ import moment from 'moment-timezone'
 
 
 global.owner = [
-  [ '50432955554', 'Eliac', true ]
+  [ 'numero', 'name', true ]
 ]; 
 
 global.suittag = ['50432955554'] 
 global.sessions = 'Sessions'
 global.jadi = 'JadiBots' 
 global.Jadibts = true
-global.packname = '🎅🎄 𝙺𝚒𝚛𝚒𝚝𝚘-𝙱𝚘𝚝 𝙼𝙳 ✨⛄';
-global.botname = '🎁 𝗞𝗜𝗥𝗜𝗧𝗢-𝗕𝗢𝗧 𝗠𝗗 ⛄★.°🦌';
-global.author = '🎄 𝑴𝒂𝒅𝒆 𝑩𝒚 𝑬𝒍𝒊𝒂𝒄 🎅❄️';
-global.dev = '🔔 © ρσɯҽɾҽԃ Ⴆყ 𝑬𝒍𝒊𝒂𝒄 🎁🎄';
-global.textbot = '🧦🎅 ᴋɪʀɪᴛᴏ-ʙᴏᴛ ᴍᴅ • 𝑬𝒍𝒊𝒂𝒄 ❄️🎄✨';
-global.etiqueta = '🎄 𝞔𝘭ⅈɑｃ 🎅';
+global.packname = ' 𝙈𝙚𝙡𝙞𝙤𝙙𝙖𝙨';
+global.botname = 'ʍɛʟɪօɖǟֆ';
+
+global.dev = ' © ρσɯҽɾҽԃ Ⴆყ ʍɛʟɪօɖǟֆ';
+global.textbot = '╰• Meliodas •╯';
 global.ch = {
-ch1: '120363403598732691@newsletter',
-ch2: '120363403598732691@newsletter',
+ch1: 'id_canal@newsletter',
+ch2: 'id_canal@newsletter',
 }
 global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
-global.kirito = 'https://kirito-my.vercel.app'
 global.moment = moment   
 
 let icono1 = [
